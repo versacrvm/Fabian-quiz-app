e0ac2822ea0884347ff9c3a1d5db16858ecb52e4
FABIAN <fabian.riess@live.de>
2024-04-19 18:24:43 +0200

    add form

---
+++ scriptForm.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.target.elements;
+  const questionValue = formElements.yourQuestion.value;
+  const answerValue = formElements.yourAnswer.value;
+  const tagValue = formElements.yourTag.value;
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  //console.log(data);
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("cardContainer");
+
+  const cardButton = document.createElement("button");
+  cardButton.classList.add("cardButton");
+  cardButton.textContent = "Hide Answer";
+
+  const questionElement = document.createElement("p");
+  questionElement.classList.add("question");
+  questionElement.textContent = questionValue;
+
+  const answerElement = document.createElement("p");
+  answerElement.classList.add("answer");
+  answerElement.textContent = answerValue;
+
+  const tagElement = document.createElement("p");
+  tagElement.classList.add("tag");
+  tagElement.textContent = tagValue;
+
+  cardContainer.appendChild(questionElement);
+  cardContainer.appendChild(answerElement);
+  cardContainer.appendChild(tagElement);
+  cardContainer.appendChild(cardButton);
+
+  document.body.appendChild(cardContainer);
+});
